def538876dacee3d559386e2af921f7100c38b04
matthewdcooper <matthew@dcooper.co.uk>
2020-01-16 09:27:24 +0000

    override default interval and break lengths via command line arguments, notify start and finish times of intervals and breaks

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+interval_length=$(( 25 * 60 ))
+break_length=$(( 5 * 60 ))
+
+# override defaults with two command line arguments
+if  [ $# -eq 2 ]; then
+	interval_length=$(( $1 * 60 ));
+	break_length=$(( $2 * 60 ));
+fi
+
 function pomo() {
-	for i in {1..4}
+	for i in {1..2}
 	do
-		notify-send -i user-available "interval $i";
-		sleep $(( 25*60 ));
-		notify-send -i user-busy "break";
-		sleep $(( 5*60 ));
+		now=$(date +%H:%M)
+		fin=$(date -d "$now today + $interval_length seconds" +%H:%M)
+		notify-send -i user-available "interval $i $now - $fin";
+		sleep $(( $interval_length ));
+
+		now=$(date +%H:%M)
+		fin=$(date -d "$now today + $interval_length seconds" +%H:%M)
+		notify-send -i user-busy "break $now - $fin";
+		sleep $(( $break_length ));
 	done;
 }
 
